51c8a6543d1f1fdbfe56ce8635d58eba24838616
Steve McMahon <steve@dcn.org>
2011-10-18 15:18:07 -0700

    Update README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='plonetheme.dtest',
       version=version,
       description="Diazo Template",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
